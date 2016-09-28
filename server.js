3ef27931a74e6ef9ed4719f3aa6bf59287a08184
eareese <eareese@eareese.com>
2016-09-28 15:01:48 -0400

    Create most basic rlyu server

---
+++ server.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Good = require('good');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+  method: 'POST',
+  path: '/rlyu',
+  handler: function (request, reply) {
+    reply("rly-u-message");
+  }
+});
+
+
+server.register({
+  register: Good,
+  options: {
+    reporters: {
+      console: [{
+        module: 'good-squeeze',
+        name: 'Squeeze',
+        args: [{
+          response: '*',
+          log: '*'
+        }]
+      }, {
+        module: 'good-console'
+      }, 'stdout']
+    }
+  }
+}, (err) => {
+
+  if (err) {
+    throw err; // something bad happened loading the plugin
+  }
+
+  server.start((err) => {
+
+    if (err) {
+      throw err;
+    }
+    server.log('info', 'Server running at: ' + server.info.uri);
+  });
+});
